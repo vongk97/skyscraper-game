b2ebd48a4189226326ea1f87a84d8f9bf79b309e
vongk97 <vongk97@gmail.com>
2021-09-20 22:26:06 -0700

    Delete src directory

--- src/org/csc133/a3/ICollider.java
+++
@@ -1,6 +0,0 @@
-package org.csc133.a3;
-
-public interface ICollider {
-    boolean collidesWith(GameObject otherObject);
-    void handleCollision(GameObject otherObject);
-}
--- src/org/csc133/a3/IStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.csc133.a3;
-
-/**
- * Provides ability for non-player helicopters to switch behaviors
- */
-public interface IStrategy {
-    public void setStrategy();
-    public void invokeStrategy();
-}
